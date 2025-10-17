510592c6ad3b8d57d22bb46d8c0454f4e47aa1dc
kamel740 <kamelelsayed740@gmail.com>
2025-10-17 03:49:08 +0300

    ff

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
