a5347f9ec8a8f3094a8a045901429d458c41f44c
kamel740 <kamelelsayed740@gmail.com>
2025-10-17 03:47:12 +0300

    gg

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav style={{ padding: "10px", backgroundColor: "#f2f2f2" }}>
+      <Link to="/" style={{ margin: "0 10px" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "0 10px" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ margin: "0 10px" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ margin: "0 10px" }}>
+        Contact
+      </Link>
+    </nav>
+  );
+};
+export default Navbar;
